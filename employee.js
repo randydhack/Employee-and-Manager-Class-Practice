b959bfbc0942ec693b9a3d91fbc129b0ee1121a0
randy hac <randydhacks@gmail.com>
2022-11-30 17:45:37 -0800

    dsds

---
+++ employee.js
@@ -0,0 +1,32 @@
+class Employee{
+
+    constructor(name, salary, title, manager = null) {
+
+        this.name = name
+        this.salary = salary
+        this.title = title
+        this.manager = manager
+
+        if (manager) {
+            manager.addEmployee(this)
+        }
+    }
+
+    _totalSubSalary() {
+        let sum = 0;
+
+        return sum;
+    }
+
+    calculateBonus(multiplier) {
+        const bonus = (this._totalSubSalary()) * multiplier
+        return bonus
+    }
+}
+
+const raph = new Employee('Raphael', 90000, 'Ninja');
+const donny = new Employee('Donatello', 85000, 'Grasshopper');
+
+console.log(raph.calculateBonus(0.25)); // => 22500
+console.log(donny.calculateBonus(0.15)); // => 12750
+module.exports = Employee
